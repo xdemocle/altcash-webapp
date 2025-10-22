15f6e9b1572e2c1f476e72e8a6dde275392a0f73
xdemocle <xdemocle@gmail.com>
2025-10-22 20:27:56 +0200

    refactor: move API-related files into dedicated /api directory

--- packages/frontend/app/api/graphql/route.ts
+++ packages/frontend/app/api/graphql/route.ts
@@ -1,15 +1,15 @@
 import { mergeResolvers } from '@graphql-tools/merge';
 import { createSchema, createYoga } from 'graphql-yoga';
 import type { NextRequest } from 'next/server';
-import resolverCount from '../../resolvers/resolver-count';
-import resolverMarkets from '../../resolvers/resolver-markets';
-import resolverMeta from '../../resolvers/resolver-meta';
-import resolverOrderQueues from '../../resolvers/resolver-order-queues';
-import resolverOrders from '../../resolvers/resolver-orders';
-import resolverPair from '../../resolvers/resolver-pair';
-import resolverSummaries from '../../resolvers/resolver-summaries';
-import resolverTickers from '../../resolvers/resolver-tickers';
-import typeDefs from '../../schema';
+import resolverCount from '../resolvers/resolver-count';
+import resolverMarkets from '../resolvers/resolver-markets';
+import resolverMeta from '../resolvers/resolver-meta';
+import resolverOrderQueues from '../resolvers/resolver-order-queues';
+import resolverOrders from '../resolvers/resolver-orders';
+import resolverPair from '../resolvers/resolver-pair';
+import resolverSummaries from '../resolvers/resolver-summaries';
+import resolverTickers from '../resolvers/resolver-tickers';
+import typeDefs from '../schema';
 
 // Build an executable GraphQL schema for Yoga
 const schema = createSchema({
