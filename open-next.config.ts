db75e66aa776bc7a20aeca9cc0c9432a03b1b25a
xdemocle <xdemocle@gmail.com>
2025-10-23 19:45:28 +0200

    feat: add KV cache purge and exclude node_modules from TypeScript compilation

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,9 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from '@opennextjs/cloudflare/config';
+import kvCachePurge from '@opennextjs/cloudflare/overrides/cache-purge/index';
 import kvIncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache';
 
 export default defineCloudflareConfig({
-  incrementalCache: kvIncrementalCache
+  incrementalCache: kvIncrementalCache,
+  cachePurge: kvCachePurge
 });
