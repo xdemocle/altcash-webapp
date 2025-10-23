872f5bfe16e7980e7a9684fe583358ca7f716c3d
xdemocle <xdemocle@gmail.com>
2025-10-23 19:03:14 +0200

    chore: remove unused r2 bucket config and import

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,5 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from '@opennextjs/cloudflare/config';
-// import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
 import kvIncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache';
 
 export default defineCloudflareConfig({
