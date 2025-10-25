2e071a91d0638ed6691e4b44a661dac5e6e03f26
xdemocle <xdemocle@gmail.com>
2025-10-25 20:23:53 +0200

    feat: enable KV cache purging and memory queue for Cloudflare config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,11 +1,14 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from '@opennextjs/cloudflare/config';
+import kvCachePurge from '@opennextjs/cloudflare/overrides/cache-purge/index';
 import kvIncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache';
+import memoryQueue from '@opennextjs/cloudflare/overrides/queue/memory-queue';
 import kvNextTagCache from '@opennextjs/cloudflare/overrides/tag-cache/kv-next-tag-cache';
 
 export default defineCloudflareConfig({
   incrementalCache: kvIncrementalCache,
-  cachePurge: 'dummy', // Disable cache purge - KV handles invalidation internally
+  cachePurge: kvCachePurge,
+  queue: memoryQueue,
   enableCacheInterception: true,
   tagCache: kvNextTagCache,
 });
