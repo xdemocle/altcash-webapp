e27759b2f69ed066ecc94f6cba0dbdcbeb28b802
xdemocle <xdemocle@gmail.com>
2025-10-25 20:25:31 +0200

    refactor: disable queue and tag cache in Cloudflare config

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,13 +2,14 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare/config';
 import kvCachePurge from '@opennextjs/cloudflare/overrides/cache-purge/index';
 import kvIncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache';
-import memoryQueue from '@opennextjs/cloudflare/overrides/queue/memory-queue';
-import kvNextTagCache from '@opennextjs/cloudflare/overrides/tag-cache/kv-next-tag-cache';
+// TODO
+// import memoryQueue from '@opennextjs/cloudflare/overrides/queue/memory-queue';
+// import kvNextTagCache from '@opennextjs/cloudflare/overrides/tag-cache/kv-next-tag-cache';
 
 export default defineCloudflareConfig({
   incrementalCache: kvIncrementalCache,
   cachePurge: kvCachePurge,
-  queue: memoryQueue,
+  // queue: memoryQueue,
   enableCacheInterception: true,
-  tagCache: kvNextTagCache,
+  // tagCache: kvNextTagCache,
 });
