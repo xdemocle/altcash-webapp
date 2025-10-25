b556277c1749ee91a9d5cfb37f89acdc194632b8
xdemocle <xdemocle@gmail.com>
2025-10-25 21:56:24 +0200

    refactor: simplify Cloudflare cache configuration

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,15 +1,9 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from '@opennextjs/cloudflare/config';
-import kvCachePurge from '@opennextjs/cloudflare/overrides/cache-purge/index';
 import kvIncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache';
-// TODO
-// import memoryQueue from '@opennextjs/cloudflare/overrides/queue/memory-queue';
-// import kvNextTagCache from '@opennextjs/cloudflare/overrides/tag-cache/kv-next-tag-cache';
 
 export default defineCloudflareConfig({
   incrementalCache: kvIncrementalCache,
-  cachePurge: kvCachePurge,
-  // queue: memoryQueue,
-  enableCacheInterception: true,
-  // tagCache: kvNextTagCache,
+  cachePurge: 'dummy', // Disable cache purge - Cloudflare API credentials not configured
+  enableCacheInterception: false, // Disable to prevent cache purge errors
 });
